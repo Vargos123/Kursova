af68e42a9315070cdbeb9f9a8c54f3e4ddfd2e4f
Vargos123 <bacahoriyk@gmail.com>
2021-03-09 19:01:38 +0200

    1

--- NotesApp/MainFormNoLogin.cs
+++ NotesApp/MainFormNoLogin.cs
@@ -366,7 +366,7 @@ namespace NotesApp
         private void button1_Click(object sender, EventArgs e)
         {
             ReloadData();
-            //// Обновляем таблицу
+            //// Обновляем таблицу!
         }
 
         private void nameBox_TextChanged(object sender, EventArgs e)
